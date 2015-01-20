dd1eb35d624b56ebb035053361ecb3f0486da6f0
mayc2 <mayc2@rpi.edu>
2015-01-20 16:11:27 -0500

    changed to main project

--- main_project/main_project/urls.py
+++ main_project/main_project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
+    # url(r'^$', 'main_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- main_project/main_project/wsgi.py
+++ main_project/main_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for app project.
+WSGI config for main_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
