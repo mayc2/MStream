bef1329d243e83f191ba5b23b8df70c41f97907a
mayc2 <mayc2@rpi.edu>
2015-01-21 07:09:39 -0500

    simple framework set up

--- main_project/accounts/views.py
+++ main_project/accounts/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Account says hello!")
+    return HttpResponse("Account says hello! <br/> <a href='/accounts/about'>About</a>")
+
+def about(request):
+    return HttpResponse("This is the about page <br/> <a href='/accounts/'>Accounts</a>")
--- main_project/main_project/urls.py
+++ main_project/main_project/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # url(r'^$', 'main_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
 ]
